fd2fce24bd90a65e7c8ae4bae39a2893405bdc82
Thiago Mattos <euthiagomattos@gmail.com>
2023-09-12 15:39:05 -0300

    created validations

--- src/main/java/com/example/challenge3/Service/CarsService.java
+++ src/main/java/com/example/challenge3/Service/CarsService.java
@@ -3,21 +3,30 @@ package com.example.challenge3.Service;
 import com.example.challenge3.Dtos.CarsDtoRequest;
 import com.example.challenge3.Dtos.CarsDtoResponse;
 import com.example.challenge3.Entity.Cars;
+import com.example.challenge3.Entity.Pilot;
 import com.example.challenge3.Repository.CarsRepository;
+import com.example.challenge3.Repository.PilotRepository;
 import com.example.challenge3.exceptions.CarNotFoundException;
+import com.example.challenge3.exceptions.DuplicateCarException;
+import com.example.challenge3.exceptions.DuplicatePilotException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static org.springframework.util.ClassUtils.isPresent;
+
 @Service
 public class CarsService {
+
+    private final PilotRepository pilotRepository;
 
     private final CarsRepository carsRepository;
 
     @Autowired
-    public CarsService(CarsRepository carsRepository) {
+    public CarsService(PilotRepository pilotRepository, CarsRepository carsRepository) {
+        this.pilotRepository = pilotRepository;
         this.carsRepository = carsRepository;
     }
 
@@ -34,12 +43,19 @@ public class CarsService {
     }
 
     public CarsDtoResponse createCar(CarsDtoRequest carsDtoRequest) {
+        if (carsRepository.findByBrandAndModel(carsDtoRequest.getBrand(), carsDtoRequest.getModel()).isPresent()) {
+            throw new DuplicateCarException("Car with the same brand and model already exists.");
+        }
+        if (carsRepository.findByName(carsDtoRequest.getPilot().getName()) != null) {
+            throw new DuplicatePilotException("Pilot with the same name already exists.");
+        }
         Cars car = convertToEntity(carsDtoRequest);
         Cars savedCar = carsRepository.save(car);
         return convertToCarsDtoResponse(savedCar);
     }
 
-    public CarsDtoResponse updateCar(String id, CarsDtoRequest carsDtoRequest) {
+
+        public CarsDtoResponse updateCar(String id, CarsDtoRequest carsDtoRequest) {
         if (carsRepository.existsById(id)) {
             Cars updatedCar = convertToEntity(carsDtoRequest);
             updatedCar.setId(id);
@@ -61,7 +77,7 @@ public class CarsService {
         carsDtoResponse.setBrand(car.getBrand());
         carsDtoResponse.setModel(car.getModel());
         carsDtoResponse.setPilot(car.getPilot());
-        carsDtoResponse.setDate(car.getYear());
+        carsDtoResponse.setYear(car.getYear());
         return carsDtoResponse;
     }
 
@@ -70,7 +86,7 @@ public class CarsService {
         car.setBrand(carsDtoRequest.getBrand());
         car.setModel(carsDtoRequest.getModel());
         car.setPilot(carsDtoRequest.getPilot());
-        car.setYear(carsDtoRequest.getDate());
+        car.setYear(carsDtoRequest.getYear());
         return car;
     }
 }
